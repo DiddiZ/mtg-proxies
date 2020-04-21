59fe2d79df1e286d2338aa25336c9b54a6beff8c
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-04-21 11:31:25 +0200

    Implemented merge_duplicates.

--- mtgproxies/decklists/__init__.py
+++ mtgproxies/decklists/__init__.py
@@ -1,5 +1,7 @@
 from mtgproxies.decklists.arena import parse_decklist_arena
+from mtgproxies.decklists.sanitizing import merge_duplicates
 
 __all__ = [
+    'merge_duplicates',
     'parse_decklist_arena',
 ]
