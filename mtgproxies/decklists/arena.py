09c2b3f87a78b620fe8e8a31c3f82022ad50fac9
Robin Kupper <robin.kupper@rwth-aachen.de>
2020-04-24 15:55:26 +0200

    Fixed reading and writing of decklists with unicode chars.

--- mtgproxies/decklists/arena.py
+++ mtgproxies/decklists/arena.py
@@ -1,3 +1,4 @@
+import codecs
 import re
 
 
@@ -15,9 +16,9 @@ def parse_decklist_arena(filepath):
         [(int, str, str, str)]: List of (count, name, set_id, collectors_number)-tuples.
     """
     decklist = []
-    with open(filepath) as fp:
+    with codecs.open(filepath, 'r', 'utf-8') as fp:
         for line in fp:
-            m = re.search(r'([0-9]+) (.+) \((.*)\) ([0-9a-z]+)', line)
+            m = re.search(r'([0-9]+) (.+) \((.*)\) ([^\s]+)', line)
             if m:
                 # Extract relevant data
                 count = int(m.group(1))
@@ -39,6 +40,6 @@ def write_decklist_arena(filepath, decklist):
     See:
         parse_decklist_arena
     """
-    with open(filepath, 'w') as f:
+    with codecs.open(filepath, 'w', 'utf-8') as f:
         for count, card_name, set_id, collector_number in decklist:
             f.write(f'{count} {card_name} ({set_id}) {collector_number}\n')
