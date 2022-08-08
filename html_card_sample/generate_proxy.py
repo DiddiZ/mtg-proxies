95a985e0373197b79f2f31c6bb5ba1d03c921a82
gbartholomeu <gabriel_bartholomeu@hotmail.com>
2022-08-08 14:51:03 -0300

    Adding HTML sample

---
+++ html_card_sample/generate_proxy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import asyncio
+from pyppeteer import launch
+import pathlib
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(f'file://{pathlib.Path().resolve()}/planeswalker.html')
+    await page.screenshot({'path': 'outputted_images/planeswalker.png', 'clip':{'x':0,'y':0, 'width': 1540, 'height': 2140} })
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
