bba47719a216c87ebb14e1fe5773c836fdfc2ebb
Robin Kupper <robin.kupper@rwth-aachen.de>
2022-10-18 00:18:13 +0200

    Added unit test for archidekt.parse_decklist

--- tests/decklist_test.py
+++ tests/decklist_test.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 
 def test_parsing():
     from mtgproxies.decklists import parse_decklist
@@ -12,3 +14,19 @@ def test_parsing():
     with open("examples/decklist.txt", "r", encoding="utf-8") as f:
         # Ignore differences in linebreaks
         assert (format(decklist, "arena") + os.linesep).replace("\r\n", "\n") == f.read().replace("\r\n", "\n")
+
+
+@pytest.mark.parametrize(
+    "archidekt_id,expected_first_card",
+    [
+        ("1212142", "Basilisk Collar"),
+        ("42", "Merieke Ri Berit"),
+    ],
+)
+def test_archidekt(archidekt_id: str, expected_first_card: str):
+    from mtgproxies.decklists.archidekt import parse_decklist
+
+    decklist, ok, _ = parse_decklist(archidekt_id)
+
+    assert ok
+    assert decklist.cards[0]["name"] == expected_first_card
